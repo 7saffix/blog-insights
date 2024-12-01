b666da3b04725b29d536a67bdc7a82763cc90032
safi <7saffix@gmail.com>
2024-12-01 22:35:34 +0600

    add update-blog route

--- Server/Controllers/blogController.js
+++ Server/Controllers/blogController.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 import Blog from "../Models/blogModel.js"
 
+//create blog
 export const createBlog = async(req,res)=>{
     const data = req.body
     if(!data.title || !data.coverImg || !data.description || !data.author || !data.authorDetails){
@@ -16,6 +17,26 @@ export const createBlog = async(req,res)=>{
     }
 }
 
+//update blog
+export const updateBlog = async(req,res)=>{
+    const {id} = req.params
+    const updatedInfo = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false,message:'invalid id'})
+    }
+
+    try {
+       const updatedData = await Blog.findByIdAndUpdate(id,updatedInfo,{new:true})
+       res.status(200).json({success:true,message:'updated successful',data:updatedData}) 
+    } catch (error) {
+        console.log('Error in update blog section',error)
+        res.status(500).json({success:false,message:'Server Error'})
+    }
+}
+
+
+//delete blog
 export const deleteBlog = async(req,res)=>{
     const {id} = req.params
 
--- Server/Routes/blogRoute.js
+++ Server/Routes/blogRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBlog, deleteBlog } from "../Controllers/blogController.js";
+import { createBlog, deleteBlog, updateBlog } from "../Controllers/blogController.js";
 
 const blogRouter  = express.Router()
 
 blogRouter.post('/post-blog',createBlog)  //create a blog
+blogRouter.put('/update-blog/:id',updateBlog)  //update a blog
 blogRouter.delete('/delete-blog/:id',deleteBlog)  //delete a blog
 
 export default blogRouter
