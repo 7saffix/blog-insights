5e65c7f5933917dcf887c5122747e526fbd2a902
Safi <130651861+7saffix@users.noreply.github.com>
2024-12-08 22:36:06 +0600

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:"http://localhost:6729"
+        target:"https://blog-insights.onrender.com/"
       }
     }
   }
